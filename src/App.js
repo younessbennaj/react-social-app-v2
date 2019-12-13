8c4bc77341b08aa3f53dddcd58e24d5e9b2c7f91
Youness BENNAJ <youness.bennaj@gmail.com>
2019-12-13 17:28:12 +0100

    add a theme provider

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import styled from 'styled-components';
 
 import Signup from './containers/Signup';
 
+import styled from 'styled-components'
+import { ThemeProvider } from 'styled-components'
+import { color } from 'styled-system'
+
+import theme from './styles/theme';
+
+const Box = styled.div`
+  ${color}
+`
+
 function Index() {
     return (
-        <div>
-            <h2>Home</h2>
-        </div>
+        <Box color="black" bg="blue">
+            <h1>Home</h1>
+        </Box>
     );
 }
 
 function Login() {
     return (
         <div>
-            <h2>Login</h2>
+            <h1>Login</h1>
         </div>
     );
 }
 
 const App = () => {
     return (
-        <Router>
-            <div>
-                <header>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/login/">Login</Link>
-                            </li>
-                            <li>
-                                <Link to="/signup/">Signup</Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </header>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <div>
+                    <header>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li>
+                                    <Link to="/login/">Login</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signup/">Signup</Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </header>
 
-                <Route path="/" exact component={Index} />
-                <Route path="/login/" component={Login} />
-                <Route path="/signup/" component={Signup} />
-            </div>
-        </Router>
+                    <Route path="/" exact component={Index} />
+                    <Route path="/login/" component={Login} />
+                    <Route path="/signup/" component={Signup} />
+                </div>
+            </Router>
+        </ThemeProvider>
     );
 }
 
