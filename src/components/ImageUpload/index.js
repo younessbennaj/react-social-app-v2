9b1508d0452e15728fae5316aeccee15476c8924
Youness BENNAJ <youness.bennaj@gmail.com>
2020-01-17 15:55:50 +0100

    upload image in edit profile component

---
+++ src/components/ImageUpload/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageUpload = () => {
+    return (
+        <div>Image Upload</div>
+    );
+}
+
+export default ImageUpload;
--- src/containers/EditProfile.js
+++ src/containers/EditProfile.js
@@ -31,23 +31,26 @@ import {
     Checkbox,
 } from '@rebass/forms/styled-components'
 
+import ImageUpload from '../components/ImageUpload';
+
 const EditProfile = ({ user: { credentials }, editUserDetails, closeModal }) => {
 
     //During the first rendering bio, location and website of credentials will
     //be undefined.
     //Because of that, the input field will become uncontrolled.
-    //Once we revceive this property (async api call), our state will be updated 
+    //Once we receive this property (async api call), our state will be updated 
     //and credentials in our props too. 
     //And at that time the input field gets converted into a controlled component;
     // that's why you are getting the error: 'A component is changing an uncontrolled input of type text 
     //to be controlled.
     //Solution: We define an empty string as default value for bio, location and website.
-    const { bio = '', location = '', website = '' } = credentials;
+    const { bio = '', location = '', website = '', imageUrl = '' } = credentials;
 
     const formik = useFormik({
-        initialValues: { bio, location, website },
+        initialValues: { bio, location, website, imageUrl: '' },
         enableReinitialize: true,
         validationSchema: Yup.object({
+            imageUrl: Yup.mixed(),
             bio: Yup.string()
                 .max(160, 'Must be 160 characters or less'),
             location: Yup.string()
@@ -56,10 +59,36 @@ const EditProfile = ({ user: { credentials }, editUserDetails, closeModal }) => 
                 .max(100, 'Must be 100 characters or less')
         }),
         onSubmit: values => {
-            editUserDetails(values);
-            closeModal();
+            // const {bio, location, website} = values;
+            console.log(values)
+            // ;            editUserDetails({bio, location, website});
+            //             closeModal();
         }
     });
+
+    const [file, setFile] = useState('');
+    const [imagePreviewUrl, setImagePreviewUrl] = useState('');
+
+    useEffect(() => {
+        setImagePreviewUrl(imageUrl);
+    }, [credentials])
+
+    const handleImageChange = (event) => {
+        event.preventDefault();
+        let reader = new FileReader();
+        const image = event.target.files[0];
+        reader.onloadend = () => {
+            setFile(image)
+            setImagePreviewUrl(reader.result)
+        }
+        reader.readAsDataURL(image)
+    }
+
+    const editProfileImage = (event) => {
+        event.preventDefault();
+        const fileInput = document.getElementById('imageUrl');
+        fileInput.click();
+    }
 
     return (
         <Box
@@ -67,6 +96,21 @@ const EditProfile = ({ user: { credentials }, editUserDetails, closeModal }) => 
             onSubmit={formik.handleSubmit}
         >
             <Flex mx={-2} mb={3} flexDirection="column">
+                {/* <ImageUpload /> */}
+                <Box px={2}>
+                    <Image width="100px" src={imagePreviewUrl} />
+                    <input
+                        id='imageUrl'
+                        name='imageUrl'
+                        type="file"
+                        onChange={handleImageChange}
+                        hidden="hidden"
+                    />
+                    <Button onClick={editProfileImage}>Upload Image</Button>
+                    {/* {formik.touched.imageUrl && formik.errors.imageUrl ? (
+                        <Box color="red">{formik.errors.imageUrl}</Box>
+                    ) : null} */}
+                </Box>
                 <Box px={2}>
                     <Label htmlFor='bio'>Bio</Label>
                     <Input
