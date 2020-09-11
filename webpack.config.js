e23089da3f52ec2ec121a9705d7641e3515667d5
Youness BENNAJ <youness.bennaj@gmail.com>
2020-09-11 14:06:45 +0200

    setup webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     entry: "./src/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
