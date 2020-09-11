aa4ae4ae3b2d0023ccfbbed8b81b5d5a662d0a97
Youness BENNAJ <youness.bennaj@gmail.com>
2020-09-11 14:10:09 +0200

    test if it works

--- src/index.js
+++ src/index.js
@@ -8,14 +8,12 @@ import store from './helpers/store';
 import App from './App';
 import reducers from './reducers';
 
-import "./styles/index.scss";
+// import "./styles/index.scss";
 
 ReactDOM.render(
-    // <Provider store={store}>
-    //     <GlobalStyles />
-    //     <App />
-    // </Provider>,
-    <div>
-        <h1>Hello World !</h1>
-    </div>
-    , document.getElementById('root'));
+    <Provider store={store}>
+        <GlobalStyles />
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
