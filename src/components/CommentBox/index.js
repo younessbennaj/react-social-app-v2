f8a46ed02d0914ac34b9db5a3042ad2d2fc1cb78
Youness BENNAJ <youness.bennaj@gmail.com>
2020-10-07 18:02:36 +0200

    mock api for post comment

--- src/components/CommentBox/index.js
+++ src/components/CommentBox/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 //Redux 
 import { connect } from 'react-redux';
@@ -24,7 +25,7 @@ import {
     Checkbox,
 } from '@rebass/forms/styled-components'
 
-const CommentBox = ({ closeModal, addComment, postId }) => {
+const CommentBox = ({ closeModal, postId }) => {
     const formik = useFormik({
         initialValues: {
             body: ''
@@ -35,7 +36,13 @@ const CommentBox = ({ closeModal, addComment, postId }) => {
         }),
         onSubmit: (values, { resetForm }) => {
 
-            addComment(values, postId);
+            console.log("add comment!");
+            console.log(postId);
+
+            axios.post(`/post/${postId}/comment`, values)
+                .then(response => {
+                    console.log(response.data);
+                })
             resetForm();
             closeModal.current.closeModal();
         },
@@ -71,6 +78,4 @@ const CommentBox = ({ closeModal, addComment, postId }) => {
     );
 }
 
-export default connect(null, {
-    addComment
-})(CommentBox);
+export default CommentBox;
---
+++ src/mocks/commentModel.js
@@ -0,0 +1,10 @@
+import avatar from "../../assets/avatar-placeholder.png";
+
+export const comment = {
+    "body": "Add a new comment",
+    "createdAt": "2020-06-24T20:45:34.121Z",
+    "userFirstName": "John",
+    "userImage": avatar,
+    "userLastName": "Doe",
+    "postId": "yuTDba987C6QWxtnjuKs"
+}
